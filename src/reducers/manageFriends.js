79b16492ecc3c83716081c5388a7c4a4f6fd3bb6
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-30 23:11:53 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,10 +2,9 @@ export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
       return {...state, friends: [...state.friends, action.friend]}
-    case 'REMOVE_FRIEND' {
+    case 'REMOVE_FRIEND':
       return {...state, friends: [...state.friends.slice(0, state.friends.findIndex(friend => friend.id === action.id))]
       }
-    }
     default:
       return state
   }
