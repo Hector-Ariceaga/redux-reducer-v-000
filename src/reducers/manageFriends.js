83cb408556d5eedb631b8c66f17f9df500994f40
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-30 22:46:46 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return {friends: {...friends, {name: action.friends} }}
+      return {friends: {...friends.push(name: action.friends)} }
     default:
       return state
   }
