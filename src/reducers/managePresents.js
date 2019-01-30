f20db522b8daf770012ff0ec7801b92586c16a5a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-30 21:24:01 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
 export function managePresents(state, action){
+  switch (action.type {
+    case 'INCREASE':
+      return {count: state.count + 1}
+    case 'DECREASE':
+      return {count: state.count - 1}
+    default:
+      return state;
+  })
 }
